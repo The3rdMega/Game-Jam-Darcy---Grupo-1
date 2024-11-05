654fa2105bedbb174a021b707239c9f4c34240e1
Inácio <Inaski.program@gmail.com>
2024-11-05 18:42:00 -0300

    Dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 60.0
+const SPEED = 15.0
 const JUMP_VELOCITY = -300.0
 const GHOST_DELAY = 5
+
 var Is_ghost = false
 var initialPosition: Vector2
 var nextTimetToTransform: float
-
+var collider: CollisionShape2D
 var animator: AnimatedSprite2D
+var canDash: bool
+var dash_timer: Timer
+
 func _ready() -> void:
 	animator = get_node("AnimatedSprite2D")
-	
+	collider = get_node("CollisionShape2D")
+	canDash = true
+	dash_timer = Timer.new()
+	add_child(dash_timer)
+	dash_timer.one_shot = true
+	dash_timer.connect("timeout",dashTimeout)
+	dash_timer.wait_time = 1.5  
 func _physics_process(delta: float) -> void:
 	
 	if(Is_ghost):
@@ -30,25 +40,29 @@ func Ghost():
 		##VIRAR HUMANO
 		position = initialPosition
 		animator.animation = "Idle"
+		collider.disabled = false
 	else:
 		##VIRAR GHOST
 		animator.animation = "Ghost"
 		initialPosition = position
+		collider.disabled = true
 	Is_ghost = !Is_ghost
-		
-
+ 
 func PlayerControl(delta:float):
 	velocity.x*=0.8
+	var dirX = 0 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	if (Input.is_key_pressed(KEY_SPACE) or Input.is_key_pressed(KEY_W) ) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	if Input.is_key_pressed(KEY_A):
 		velocity.x += -SPEED
+		dirX = -1
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += SPEED
+		dirX = 1
+	if Input.is_key_pressed(KEY_SHIFT) and canDash:
+		dash(dirX)
 		
 	move_and_slide()
 func GhostControl():
@@ -61,5 +75,15 @@ func GhostControl():
 		velocity.y += -SPEED/2
 	if Input.is_key_pressed(KEY_S):
 		velocity.y += SPEED/2
+		
 	move_and_slide()
+
+func dash(direction:int):
+	print("shimishimeishimeishimiai drake swalalalala")
+	velocity.x+= (30*SPEED*direction)
+	dash_timer.start()
+	canDash = false	
 	
+func dashTimeout():
+	canDash = true
+	print("not")
