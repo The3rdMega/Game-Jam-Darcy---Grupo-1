91a5d4fb248386d4b36b6aec15f43bfc8e497d63
Inaskii <Inaski.program@gmail.com>
2024-11-05 18:50:43 -0300

    dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 60.0
+const SPEED = 15.0
 const JUMP_VELOCITY = -300.0
 const GHOST_DELAY = 5
 var Is_ghost = false
 var initialPosition: Vector2
 var nextTimetToTransform: float
 
-
+var canDash: bool
+var dash_timer: Timer
+var collider:CollisionShape2D
 var rising = false
 var animator: AnimatedSprite2D
 func _ready() -> void:
 	animator = get_node("AnimatedSprite2D")
-	
+	collider = get_node("CollisionShape2D")
+	canDash = true
+	dash_timer = Timer.new()
+	add_child(dash_timer)
+	dash_timer.one_shot = true
+	dash_timer.connect("timeout",dashTimeout)
+	dash_timer.wait_time = 1.5  
+
 func _physics_process(delta: float) -> void:
 	
 	if(Is_ghost):
@@ -40,10 +49,10 @@ func Ghost():
 		spawnDead.global_position += Vector2(0, -6)
 		animator.animation = "Ghost"
 		initialPosition = position
-		get_node("CollisionShape2D").disabled = true
+		collider.disabled = true
 	else:
 		##VIRAR HUMANO
-		get_node("CollisionShape2D").disabled = false
+		collider.disabled = false
 		rising = true
 		spawnDead.queue_free()
 		position = initialPosition
@@ -53,7 +62,8 @@ func Ghost():
 		
 
 func PlayerControl(delta:float):
-	velocity.x*=0.5
+	velocity.x*=0.8
+	var dirX = 0 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -62,9 +72,14 @@ func PlayerControl(delta:float):
 
 	if Input.is_key_pressed(KEY_A):
 		velocity.x += -SPEED
+		dirX = -1
+
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += SPEED
-		
+		dirX = 1
+	if Input.is_key_pressed(KEY_SHIFT) and canDash:
+		dash(dirX)
+
 	move_and_slide()
 func GhostControl():
 	velocity*=0.8
@@ -77,14 +92,18 @@ func GhostControl():
 	if Input.is_key_pressed(KEY_S):
 		velocity.y += SPEED/2
 	move_and_slide()
-	
-
-
-
-
 
 func _on_animated_sprite_2d_animation_finished():
 	print("ENTERED")
 	if 	$AnimatedSprite2D.animation == "Rise":
 		rising = false
 		$AnimatedSprite2D.play("Idle")
+func dash(direction:int):
+	print("shimishimeishimeishimiai drake swalalalala")
+	velocity.x+= (30*SPEED*direction)
+	dash_timer.start()
+	canDash = false	
+	
+func dashTimeout():
+	canDash = true
+	print("not")
