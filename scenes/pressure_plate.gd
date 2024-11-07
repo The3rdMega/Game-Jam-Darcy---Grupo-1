f6261e09305fd9424cde9f3d97b85e0e58d168a1
Arthur <arthur.bispo4@gmail.com>
2024-11-07 14:41:01 -0300

    update level 2

--- scenes/pressure_plate.gd
+++ scenes/pressure_plate.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
-	get_parent().OpenDoor()
+	get_parent().SetNum(1)
 
 
 func _on_area_exited(area: Area2D) -> void:
-	get_parent().CloseDoor()
+	get_parent().SetNum(-1)
